413faf323a37843ce2e6ad0c8f4ebbc5fc50407b
ClearT <clear1014@gmail.com>
2022-11-28 21:10:54 +0900

    set alarm page prototype

--- lib/alarm.dart
+++ lib/alarm.dart
@@ -8,16 +8,82 @@ class AlarmPage extends StatefulWidget {
   State<AlarmPage> createState() => _AlarmPageState();
 }
 
+
 class _AlarmPageState extends State<AlarmPage> {
+
+  final alarmInfoList = [
+    AlarmInfo( "test alarm 1", 12, 30, true),
+    AlarmInfo( "test alarm 2", 12, 30, false),
+  ];
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Alarm'),
       ),
-      body: Container(
-        child: const Text('Alarm'),
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Delete Alarm'),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/lookupSelection', arguments: {});
+                    },
+                  child: Text('Add new Alarm'),
+                ),
+              ],
+            ),
+          ),
+          ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: alarmInfoList.length,
+            itemBuilder: (context,index){
+              return Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(alarmInfoList[index].name),
+                          Text(
+                            '${alarmInfoList[index].hour.toString()}:${alarmInfoList[index].minute.toString()}',
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      IconButton(
+                        icon: alarmInfoList[index].isActive
+                            ? Icon(Icons.alarm, color: Colors.deepOrange)
+                            : Icon(Icons.alarm_off),
+                        onPressed: () {
+                          setState(() {
+                            alarmInfoList[index].isActive = !alarmInfoList[index].isActive;
+                          });
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          )
+        ],
       ),
       drawer: Drawer(
           child:SingleChildScrollView(
@@ -31,4 +97,18 @@ class _AlarmPageState extends State<AlarmPage> {
       ),
     );
   }
+}
+
+class AlarmInfo {
+  String name;
+  int hour;
+  int minute;
+  bool isActive;
+
+  AlarmInfo (
+    this.name,
+    this.hour,
+    this.minute,
+    this.isActive
+  );
 }
